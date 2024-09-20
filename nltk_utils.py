df5b21ef2572a48367d887a2913fd490596f15af
jsavinong <j.savinong@gmail.com>
2024-09-20 13:15:31 -0400

    model.py creation file

--- nltk_utils.py
+++ nltk_utils.py
@@ -2,14 +2,48 @@ import nltk
 #nltk.download('punkt')
 #nltk.download('punkt_tab')
 from nltk.stem.snowball import SnowballStemmer
+import numpy as np
 
 stemmer = SnowballStemmer('spanish')
 def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
     return nltk.word_tokenize(sentence)
 
 def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organiza", "organizar", "organizando"]
+    words = [stem(w) for w in words]
+    -> ["organiza", "organiza", "organiza"]
+    """
     return stemmer.stem(word.lower())
 
-def bag_of_words(tokenized_sentence, all_words):
-    pass
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["Hola", "En", "qué", "te", "puedo", "ayudar"]
+    words = ["Hola", "amigo", "qué", "puedo", "hacer", "para", "ayudarte"]
+    bow   = [  1 ,    0 ,    1 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # stemmed_words = [stem(w) for w in words]  # Stem the words list as well
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        print(w, sentence_words )
+        if w in sentence_words: 
+            bag[idx] = 1
 
+    return bag
+
+sentence = ["Hola", "En", "qué", "te", "puedo", "ayudar"]
+words = ["Hola", "amigo", "qué", "puede", "hacer", "para", "ayudarte"]
+bow = bag_of_words(sentence, words)
+print(bow)
--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
+
 
 with open('intents.json', 'r', encoding='utf-8') as f:
     intents = json.load(f)
@@ -33,3 +38,24 @@ for (pattern_sentence, tag) in xy:
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    # dataset[idx]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
+
+# Hyperparameters
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
+
+model = NeuralNet()
